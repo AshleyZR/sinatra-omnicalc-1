2fc18057a4e445d080c6dcba40f218f6f9c96023
Ziyu (Ashley) Ren <92663054+AshleyZR@users.noreply.github.com>
2023-10-25 00:34:48 +0000

    finished sinatra omnicalc

--- app.rb
+++ app.rb
@@ -6,46 +6,49 @@ get("/") do
 end
 
 get ("/square/new") do
-  number = params["number"].to_i
-  redirect to("/square/results/#{number}")
   erb(:square_page)
 end
 
-get ("/square/results/:number") do
+get ("/square/results") do
   @num = params["number"]
   @square = @num.to_f * @num.to_f
   erb(:square_result_page)
 end
 
 get("/square_root/new") do
-  number = params["user_number"].to_i
   erb(:square_root_page)
-  redirect to("/square_root/results/#{number}")
 end
 
-get("/square_root/results/:number") do
-  @num = params["number"]
+get("/square_root/results") do
+  @num = params["user_number"]
   @square_root = @num.to_f**0.5
   erb(:square_root_result_page)
 end
 
 get("/payment/new") do
-  apr = params["user_apr"].to_i 
-  years = params["user_years"].to_i
-  pv = params['user_pv'].to_i
   erb(:payment_page)
-  redirect to("/payment/results/#{apr}/#{years}/#{pv}")
 end
 
-get("/payment/results/:apr/:years/:pv") do
-  @apr = (params['apr'].to_f)/100
-  @years = params['years']
-  @pv = params['pv'].to_f
+get("/payment/results") do
+  @apr = (params['user_apr'].to_f)
+  @years = params['user_years'].to_i
+  @pv = params['user_pv'].to_f
   periods = @years * 12
-  percentage_per_period = @apr/12
+  percentage_per_period = (@apr/12)/100
   numeratr = percentage_per_period * @pv
   denominatr = 1 - ((1+percentage_per_period.to_f)**(-periods.to_f))
   @payment = numeratr.to_f/denominatr.to_f
   erb(:payment_result_page)
 end
 
+get("/random/new") do
+  erb(:random_page)
+end
+
+get("/random/results") do
+  @mini = params['user_min'].to_f
+  @maxi = params['user_max'].to_f
+  @rand = rand(@mini..@maxi)
+  erb(:random_result_page)
+end
+  
